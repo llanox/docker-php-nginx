79a3d216dd1900a86ec8499042eba17391569562
Juan Gabriel Gutierrez <correo.del.llanox@gmail.com>
2016-01-04 11:25:15 -0500

    Allow pool to run as root (disabled by default)
     Allow pool to run as root (disabled by default)

--- php-fpm.sh
+++ php-fpm.sh
@@ -2,4 +2,4 @@
 exec 1> /proc/1/fd/1
 exec 2> /proc/1/fd/2
 sed -i -e "s/^user.*=.*$/user = ${PHP_USER}/" /usr/local/etc/php-fpm.d/*
-exec php-fpm --force-stderr
+exec php-fpm --force-stderr --allow-to-run-as-root
